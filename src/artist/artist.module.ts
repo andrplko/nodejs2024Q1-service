7b5f89269a0faf6ebc7325058e186aa7714c2d7a
Andrei Paleshka <andrepoleshko@gmail.com>
2024-03-11 19:10:20 +0100

    feat: implement artist entity

---
+++ src/artist/artist.module.ts
@@ -0,0 +1,18 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { ArtistController } from './artist.controller';
+import { FavoritesModule } from 'src/favorites/favorites.module';
+import { AlbumModule } from 'src/album/album.module';
+import { TrackModule } from 'src/track/track.module';
+
+@Module({
+  imports: [
+    forwardRef(() => FavoritesModule),
+    forwardRef(() => AlbumModule),
+    forwardRef(() => TrackModule),
+  ],
+  controllers: [ArtistController],
+  providers: [ArtistService],
+  exports: [ArtistService],
+})
+export class ArtistModule {}
