4de53aca2b4c729353a3777642bf392da5f7e925
Andrei Paleshka <andrepoleshko@gmail.com>
2024-03-22 17:13:57 +0100

    feat: add typeorm column decorators to track entity

--- src/track/entities/track.entity.ts
+++ src/track/entities/track.entity.ts
@@ -1,7 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Track')
 export class Track {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column('uuid', { nullable: true })
   artistId?: string | null;
+
+  @Column('uuid', { nullable: true })
   albumId?: string | null;
+
+  @Column('int')
   duration: number;
 }
