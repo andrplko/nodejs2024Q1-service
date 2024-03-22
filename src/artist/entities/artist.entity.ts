747e7f8d2aca0f4efcdfcbce185cf4ac0fc5e525
Andrei Paleshka <andrepoleshko@gmail.com>
2024-03-22 17:09:32 +0100

    feat: add typeorm column decorators to artist entity

--- src/artist/entities/artist.entity.ts
+++ src/artist/entities/artist.entity.ts
@@ -1,5 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Artist')
 export class Artist {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column('boolean')
   grammy: boolean;
 }
