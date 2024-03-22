53ad60797bab65444acf6a26d1c4e9849d76e298
Andrei Paleshka <andrepoleshko@gmail.com>
2024-03-22 17:08:55 +0100

    feat: add typeorm column decorators to album entity

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Album')
 export class Album {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column('int')
   year: number;
+
+  @Column('uuid', { nullable: true })
   artistId?: string | null;
 }
