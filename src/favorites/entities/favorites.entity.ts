541ac665f57974b9eaff24762d0ed0712572c835
Andrei Paleshka <andrepoleshko@gmail.com>
2024-03-22 17:12:15 +0100

    feat: add typeorm column decorators and relations to favorites entity

--- src/favorites/entities/favorites.entity.ts
+++ src/favorites/entities/favorites.entity.ts
@@ -1,9 +1,22 @@
+import { Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Album } from 'src/album/entities/album.entity';
 import { Artist } from 'src/artist/entities/artist.entity';
 import { Track } from 'src/track/entities/track.entity';
 
+@Entity('Favorites')
 export class Favorites {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToMany(() => Artist)
+  @JoinTable()
   artists: Artist[];
+
+  @ManyToMany(() => Album)
+  @JoinTable()
   albums: Album[];
+
+  @ManyToMany(() => Track)
+  @JoinTable()
   tracks: Track[];
 }
